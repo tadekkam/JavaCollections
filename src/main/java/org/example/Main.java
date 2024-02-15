8204af0c3bf91348beb3e574ac6e0acc186e72e3
Tadeusz Kamyszek <tkamyszek@op.pl>
2024-02-15 19:55:29 +0100

    Task 4_2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,20 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Pair<Integer, String> pair = new Pair<>(1, "first");
+        System.out.println(pair);
+
+        pair.setFirst(2);
+        pair.setSecond("second");
+        System.out.println(pair);
+
+        Integer firstElement = pair.getFirst();
+        String secondElement = pair.getSecond();
+
+        System.out.println(firstElement + " " + secondElement);
+
     }
 }
